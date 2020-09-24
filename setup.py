812a1d88560524bfe37c888dd3763dce7eb5b96e
codewithpatch <chesterdealday@gmail.com>
2020-09-24 20:51:28 +0200

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='scrape_agency_email',
+    version='0.1.0',
+    packages=['scrape_agency_email', 'scrape_agency_email.spiders', 'scrape_agency_email.scrape_agency_email',
+              'scrape_agency_email.scrape_agency_email.spiders'],
+    url='',
+    license='',
+    author='Chester de Alday',
+    author_email='chesterdealday@gmail.com',
+    description='This project goals to scrape agency email from a list of agency names in www.healthdirect.gov.au'
+)
